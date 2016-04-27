1d665538516aac0c9902c7055ace4447ff65191a
abe <abe@thehonestape.com>
2016-04-27 15:16:49 -0400

    organize navbar

--- header.php
+++ header.php
@@ -25,52 +25,59 @@
 	<?php do_action( 'before' ); ?>
 
 
-<nav class="site-navigation">
-<?php // substitute the class "container-fluid" below if you want a wider content area ?>
-	<div class="container">
-		<div class="row">
-			<div class="site-navigation-inner col-sm-12">
-				<div class="navbar navbar-default">
-					<div class="navbar-header">
-							<!-- <ul>
-								<li>Home</li>
-								<li>Projects</li>
-								<li>About Me</li>
-								<li>Blog</li>
-								<li>Contact</li>
-						 </div> -->
+	<!-- ******************* The Navbar Area ******************* -->
+	<div class="wrapper-fluid wrapper-navbar" id="wrapper-navbar">
 
+			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'understrap' ); ?></a>
 
+			<nav class="site-navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
 
-						<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-							<span class="sr-only"><?php _e('Toggle navigation','_tk') ?> </span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
+					<div class="navbar navbar-default">
 
-						<!-- Your site title as branding in the menu -->
+							<div class="container">
 
-					<!-- The WordPress Menu goes here -->
-					<?php wp_nav_menu(
-						array(
-							'theme_location' 	=> 'primary',
-							'depth'             => 2,
-							'container'         => 'div',
-							'container_class'   => 'collapse navbar-collapse',
-							'menu_class' 		=> 'nav navbar-nav',
-							'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
-							'menu_id'			=> 'main-menu',
-							'walker' 			=> new wp_bootstrap_navwalker()
-						)
-					); ?>
+									<div class="row">
 
-				</div><!-- .navbar -->
-			</div>
-		</div>
-	</div><!-- .container -->
-</nav><!-- .site-navigation -->
+											<div class="col-xs-12">
+
+													<div class="navbar-header">
+
+															<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
+															<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+																	<span class="sr-only">Toggle navigation</span>
+																	<span class="icon-bar"></span>
+																	<span class="icon-bar"></span>
+																	<span class="icon-bar"></span>
+															</button>
+
+															<!-- Your site title as branding in the menu -->
+															<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+
+													</div>
+
+													<!-- The WordPress Menu goes here -->
+													<?php wp_nav_menu(
+																	array(
+																			'theme_location' => 'primary',
+																			'container_class' => 'collapse navbar-collapse navbar-responsive-collapse',
+																			'menu_class' => 'nav navbar-nav',
+																			'fallback_cb' => '',
+																			'menu_id' => 'main-menu',
+																			'walker' => new wp_bootstrap_navwalker()
+																	)
+													); ?>
+
+											</div> <!-- .col-md-11 or col-md-12 end -->
+
+									</div> <!-- .row end -->
+
+							</div> <!-- .container -->
+
+					</div><!-- .navbar -->
+
+			</nav><!-- .site-navigation -->
+
+	</div><!-- .wrapper-navbar end -->
 
 <div class="main-content">
 <?php // substitute the class "container-fluid" below if you want a wider content area ?>
